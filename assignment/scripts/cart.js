798bd6c441ed1582bd5b4960774e2565f6464dab
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-22 17:41:51 -0500

    Function addItem created

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,3 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = []; //created global variable named basket
+function addItem (item){
+    console.log('In addItem',item); // shows the item going to be added to array
+        basket.push(item); //adds new item to end of array
+return true;
+}
